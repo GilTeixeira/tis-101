14e52ce658fcaa487ea4bb54906cba1d00568c93
Gil Teixeira <gil.teixeira@fe.up.pt>
2022-05-29 02:20:27 +0100

    Add starter code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,67 @@
+/* eslint max-classes-per-file: 0 */ // --> OFF
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+function Square(): JSX.Element {
+    return (
+        <button type="button" className="square">
+            {/* TODO */}
+        </button>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+class Board extends React.Component {
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, class-methods-use-this
+    renderSquare(i: number): JSX.Element {
+        return <Square />
+    }
+
+    render(): JSX.Element {
+        const status = 'Next player: X'
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.PureComponent {
+    render(): JSX.Element {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+// ========================================
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!)
+root.render(<Game />)
